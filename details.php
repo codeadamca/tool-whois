1e9c59adccb3739f3da3d8c85ece9acc37c30012
Adam Thomas <thomasadam83@hotmail.com>
2025-01-29 01:06:17 -0500

    Added design

--- details.php
+++ details.php
@@ -22,45 +22,84 @@ die();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WHOIS Results for <?=strtoupper($domain);?></title>
+    <title>WHOIS | CodeAdam</title>
+
+    <link
+      href="https://fonts.googleapis.com/css?family=Lora"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+
+    <link rel="stylesheet" href="https://cdn.codeadam.ca/tools@1.0.0/w3.css" />
 </head>
 <body>
+
+
+    <div class="w3-container w3-margin-top w3-center" style="max-width: 400px; margin: auto;">
+
+        <a href="/">
+            <img src="https://cdn.codeadam.ca/images@1.0.0/codeadam-logo-coloured.png" width="80">
+        </a>
+
+        <h1>WHOIS</h1>
     
-    <h1>WHOIS Results for <?=strtoupper($domain);?></h1>
+        <h2><?=strtolower($domain);?></h2>
 
-    <h2>Name Servers</h2>
+        <div class="w3-left-align">
+                
+            <hr>
 
-    <p>
-        <?php foreach($response['Name Server'] as $server): ?>
-            <?=$server?>
-            <br>
-        <?php endforeach; ?>
-    </p>
+            <h3>Name Servers</h3>
 
-    <h2>Domain Details</h2>
+            <p>
+                <?php foreach($response['Name Server'] as $server): ?>
+                    <?=$server?>
+                    <br>
+                <?php endforeach; ?>
+            </p>
 
-    <p>
-        Registry Domain ID: <?=$response['Registry Domain ID']?>
-        <br>
-        Registrar WHOIS Server: <?=$response['Registrar WHOIS Server']?>
-        <br>
-        Registrar URL: <?=$response['Registrar URL']?>
-        <br>
-        Updated Date: <?=$response['Updated Date']?>
-        <br>
-        Created Date: <?=$response['Created Date']?>
-        <br>
-        Expiry Date: <?=$response['Expiry Date']?>
-        <br>
-        Registrar: <?=$response['Registrar']?>
-        <br>
-        Registrar IANA ID: <?=$response['Registrar IANA ID']?>
-        <br>
-        Registrar Abuse Contact Email: <?=$response['Registrar Abuse Contact Email']?>
-        <br>
-        Registrar Abuse Contact Phone: <?=$response['Registrar Abuse Contact Phone']?>
-    </p>
+            <hr>
 
-</body>
+            <h3>Domain Details</h3>
+
+            <p>
+                Registry Domain ID: <?=$response['Registry Domain ID']?>
+                <br>
+                Registrar WHOIS Server: <?=$response['Registrar WHOIS Server']?>
+                <br>
+                Registrar URL: <?=$response['Registrar URL']?>
+                <br>
+                Updated Date: <?=$response['Updated Date']?>
+                <br>
+                Created Date: <?=$response['Created Date']?>
+                <br>
+                Expiry Date: <?=$response['Expiry Date']?>
+                <br>
+                Registrar: <?=$response['Registrar']?>
+                <br>
+                Registrar IANA ID: <?=$response['Registrar IANA ID']?>
+                <br>
+                Registrar Abuse Contact Email: <?=$response['Registrar Abuse Contact Email']?>
+                <br>
+                Registrar Abuse Contact Phone: <?=$response['Registrar Abuse Contact Phone']?>
+            </p>
+
+            <hr>
+            
+        </div>
+
+        <a href="/">Lookup Another Domain</a>
+    
+    </div>
+    
+    <script
+      src="https://kit.fontawesome.com/57a8a8c892.js"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
 
--- index.php
+++ index.php
@@ -3,26 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>WHOIS | CodeAdam</title>
+
+    <link
+      href="https://fonts.googleapis.com/css?family=Lora"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+
+    <link rel="stylesheet" href="https://cdn.codeadam.ca/tools@1.0.0/w3.css" />
 </head>
 <body>
 
-    <?php if(isset($_GET['error'])): ?>
+    <div class="w3-container w3-margin-top w3-center" style="max-width: 400px; margin: auto;">
+
+        <a href="/">
+            <img src="https://cdn.codeadam.ca/images@1.0.0/codeadam-logo-coloured.png" width="80">
+        </a>
+
+        <h1>WHOIS</h1>
+
+        <?php if(isset($_GET['error'])): ?>
+        
+            <p>There was an error with the provided domain.</p>
+
+        <?php elseif(isset($_GET['api'])): ?>
+        
+            <p>There was an error with the WHOIS lookup.</p>
+
+        <?php endif; ?>
+
+        <form method="post" action="whois.php" autocomplete="off">
+
+            <input type="text" 
+                name="domain" 
+                placeholder="codeadam.ca" 
+                class="w3-input w3-margin-bottom"
+                autocomplete="off"
+            >
+            <input type="submit" value="SUBMIT" class="w3-button" style="background-color: var(--ca-red);">
+
+        </form>
+
+    </div>
     
-        <p>There was an error with the provided domain.</p>
-
-    <?php elseif(isset($_GET['api'])): ?>
-    
-        <p>There was an error with the WHOIS lookup.</p>
-
-    <?php endif; ?>
-
-    <form method="post" action="whois.php">
-
-        <input type="text" name="domain" placeholder="codeadam.ca">
-        <input type="submit" value="WHOIS">
-
-    </form>
-    
-</body>
+    <script
+      src="https://kit.fontawesome.com/57a8a8c892.js"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
